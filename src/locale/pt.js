6ea0809781391075bc34aa97fbc1d01ebcbd302b
willmustafa <willian2142@gmail.com>
2022-09-03 20:09:36 -0300

    fonts and localization

--- src/locale/pt.js
+++ src/locale/pt.js
@@ -2,9 +2,9 @@ export default {
   home: {
     jumbo: "Olá, meu nome é",
     headline:
-      "Sou um engenheiro de software em busca de automatizar o dia a dia.",
+      "Sou um engenheiro de software em busca de automatizar o dia a dia. ",
     working:
-      "Trabalhando atualmente como Desenvolvedor Full Stack na Integra.do.",
+      "Trabalhando atualmente como <b>Desenvolvedor Full Stack na <a href='https://integra.do/' target='_blank'>Integra.do</a></b>",
   },
   nav: {
     home: "Home",
