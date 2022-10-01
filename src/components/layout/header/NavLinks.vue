25685c52b41e57ad00f5223bb5c98c99768e437d
willmustafa <willian2142@gmail.com>
2022-10-01 19:21:43 -0300

    Change nav order

--- src/components/layout/header/NavLinks.vue
+++ src/components/layout/header/NavLinks.vue
@@ -4,10 +4,10 @@
       <router-link to="#home">{{ $t("nav.home") }}</router-link>
     </li>
     <li>
-      <router-link to="#portfolio">{{ $t("nav.portfolio") }}</router-link>
+      <router-link to="#about">{{ $t("nav.about") }}</router-link>
     </li>
     <li>
-      <router-link to="#about">{{ $t("nav.about") }}</router-link>
+      <router-link to="#portfolio">{{ $t("nav.portfolio") }}</router-link>
     </li>
     <li>
       <router-link to="#contact">{{ $t("nav.contact") }}</router-link>
